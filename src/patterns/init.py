fd02986ffbef47b50f3462183eb3d15fdad9453f
Mohsen-data-wizard <mohsen.mvil@gmail.com>
2025-06-12 23:56:32 -0700

    Delete src/patterns directory

--- src/patterns/init.py
+++
@@ -1,9 +0,0 @@
-
-
-"""
-ماول الگوهای regex
-"""
-
-from .improved_patterns import ImprovedRegexPatterns
-from .regex_patterns import PatternManager
-__all__ = ['ImprovedRegexPatterns' , 'PatternManager' , ]
